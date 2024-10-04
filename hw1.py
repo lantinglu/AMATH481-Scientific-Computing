e86dce4ba8263d376a86dd2193ebb559a1d520a6
llu3 <llu3@uw.edu>
2024-10-03 21:09:57 -0700

    Initial commit

--- hw1.py
+++ hw1.py
@@ -50,24 +50,23 @@ print("A2 = ", A2)
 print("A3 = ", A3)
 
 #Problem 2
-A = np.array([[1, 2], [-1, 1]])
-B = np.array([[2, 0], [0, 2]])
-C = np.array([[2, 0, -3], [0, 0, -1]])
-D = np.array([[1, 2], [2, 3], [-1, 0]])
-x = np.array([[1], [0]])
-y = np.array([[0], [1]])
-z = np.array([[1], [2], [-1]])
+A = np.array([[1,2], [-1,1]])
+B = np.array([[2,0], [0,2]])
+C = np.array([[2,0,-3], [0,0,-1]])
+D = np.array([[1,2],[2,3], [-1,0]])
+x= np.array([1,0])
+y = np.array([0,1])
+z = np.array([1,2,-1])
 
 A4 = A + B
-A5 = (3 * x - 4 * y).flatten()
-A6 = (A @ x).flatten()
-A7 = (B @ (x - y)).flatten()
-A8 = (D @ x).flatten()
-A9 = (D @ y + z).flatten()
+A5 = (3 * x - 4 * y)
+A6 = (A @ x)
+A7 = (B @ (x - y))
+A8 = (D @ x)
+A9 = (D @ y + z)
 A10 = A @ B
 A11 = B @ C
 A12 = C @ D
-
 
 print("A4 = ", A4)
 print("A5 = ", A5)
